2474035e5cc6c2614460f2fae3da1332188ed5eb
longvalery <longvalery@gmail.com>
2024-02-19 22:04:06 +0300

    Made trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let symbol = '';
+    let count = 0;
+    let result = '';
+    if ((size === 0) || (string.length == 0) ) { return result; }
+    if (size == undefined) { return string; }
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == symbol) {
+            count ++;
+            if (count < size) {result += string[i];}
+                                 }
+        else {
+            symbol = string[i];
+            count = 0;
+            result += string[i];
+             }                         
+                                            }
+    return result;
+                                        }
 
-}
+/* for developer test
+console.log(trimSymbols('xxx', 3)); // 'xxx' - ничего не удалили т.к разрешено 3 символа подряд
+console.log(trimSymbols('xxx', 2)); // 'xx' - удалили один символ
+console.log(trimSymbols('xxx', 1)); // 'x'
+console.log(trimSymbols('xxxaaaaa', 2)); // 'xxaa'
+console.log(trimSymbols('xxxaaaaab', 3)); // 'xxxaaab'
+*/
